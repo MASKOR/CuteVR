e5ece987b9de1204161c3ad67168ac226ee92fc4
Marcus Meeßen <marcus@meessen.online>
2018-09-03 19:12:20 +0200

    m/c/components/output/speaker
    patch:moved to typed component
    
    patch:removed shared namespace
    
    patch:renamed type because of name clashes
    
    patch:new categorized interface
    
    patch:merged shared into core module

---
+++ modules/Core/include/CuteVR/Components/Output/Speaker.hpp
@@ -0,0 +1,18 @@
+/// @file
+/// @author Marcus Meeßen
+/// @copyright Copyright (c) 2017-2018 Marcus Meeßen
+/// @copyright Copyright (c) 2018      MASKOR Institute FH Aachen
+
+#ifndef CUTE_VR_COMPONENTS_OUTPUT_SPEAKER
+#define CUTE_VR_COMPONENTS_OUTPUT_SPEAKER
+
+#include <CuteVR/Component.hpp>
+
+namespace CuteVR { namespace Components { namespace Output {
+    /// @brief This class is currently only a placeholder.
+    struct Speaker :
+            public CategorizedComponent<Component::Category::output, Component> {
+    };
+}}}
+
+#endif // CUTE_VR_COMPONENTS_OUTPUT_SPEAKER
