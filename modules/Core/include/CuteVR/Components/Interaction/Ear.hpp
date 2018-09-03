790fce2b10bc4e6d0fe114bc859762b5bd067a68
Marcus Meeßen <marcus@meessen.online>
2018-09-03 16:17:29 +0200

    m/c/components/interaction/ear
    patch:moved to typed component
    
    patch:removed shared namespace
    
    patch:renamed type because of name clashes
    
    patch:new categorized interface
    
    patch:merged shared into core module

---
+++ modules/Core/include/CuteVR/Components/Interaction/Ear.hpp
@@ -0,0 +1,18 @@
+/// @file
+/// @author Marcus Meeßen
+/// @copyright Copyright (c) 2017-2018 Marcus Meeßen
+/// @copyright Copyright (c) 2018      MASKOR Institute FH Aachen
+
+#ifndef CUTE_VR_COMPONENTS_INTERACTION_EAR
+#define CUTE_VR_COMPONENTS_INTERACTION_EAR
+
+#include <CuteVR/Component.hpp>
+
+namespace CuteVR { namespace Components { namespace Interaction {
+    /// @brief This class is currently only a placeholder.
+    struct Ear :
+            public CategorizedComponent<Component::Category::interaction, Component> {
+    };
+}}}
+
+#endif // CUTE_VR_COMPONENTS_INTERACTION_EAR
