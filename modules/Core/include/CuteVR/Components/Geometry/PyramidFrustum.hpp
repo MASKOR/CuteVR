7c4cd8f772a22595cc42a41fd01ff3b5d2003a56
Marcus Meeßen <marcus.meessen@alumni.fh-aachen.de>
2018-10-16 15:46:34 +0200

    m/c/components/geometry/pyramidFrustum
    patch:new categorized interface
    
    patch:merged shared into core module

---
+++ modules/Core/include/CuteVR/Components/Geometry/PyramidFrustum.hpp
@@ -0,0 +1,18 @@
+/// @file
+/// @author Marcus Meeßen
+/// @copyright Copyright (c) 2017-2018 Marcus Meeßen
+/// @copyright Copyright (c) 2018      MASKOR Institute FH Aachen
+
+#ifndef CUTE_VR_COMPONENTS_GEOMETRY_PYRAMID_FRUSTUM
+#define CUTE_VR_COMPONENTS_GEOMETRY_PYRAMID_FRUSTUM
+
+#include <CuteVR/Component.hpp>
+
+namespace CuteVR { namespace Components { namespace Geometry {
+    /// @brief This class is currently only a placeholder.
+    struct PyramidFrustum :
+            public CategorizedComponent<Component::Category::geometry, Component> {
+    };
+}}}
+
+#endif // CUTE_VR_COMPONENTS_GEOMETRY_PYRAMID_FRUSTUM
