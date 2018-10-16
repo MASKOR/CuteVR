8a2941e428ed11eaa0eed32a05653c2fb576bf12
Marcus Meeßen <marcus.meessen@alumni.fh-aachen.de>
2018-10-16 15:44:14 +0200

    m/c/components/geometry/ellipsoid
    patch:new categorized interface
    
    patch:merged shared into core module

---
+++ modules/Core/include/CuteVR/Components/Geometry/Ellipsoid.hpp
@@ -0,0 +1,18 @@
+/// @file
+/// @author Marcus Meeßen
+/// @copyright Copyright (c) 2017-2018 Marcus Meeßen
+/// @copyright Copyright (c) 2018      MASKOR Institute FH Aachen
+
+#ifndef CUTE_VR_COMPONENTS_GEOMETRY_ELLIPSOID
+#define CUTE_VR_COMPONENTS_GEOMETRY_ELLIPSOID
+
+#include <CuteVR/Component.hpp>
+
+namespace CuteVR { namespace Components { namespace Geometry {
+    /// @brief This class is currently only a placeholder.
+    struct Ellipsoid :
+            public CategorizedComponent<Component::Category::geometry, Component> {
+    };
+}}}
+
+#endif // CUTE_VR_COMPONENTS_GEOMETRY_ELLIPSOID
