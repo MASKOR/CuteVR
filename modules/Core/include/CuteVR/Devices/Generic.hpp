f383e6d0cb7c85eeed1ff1e6d8d3a181b3c40b9d
Marcus Meeßen <marcus@meessen.online>
2018-10-29 11:36:13 +0100

    m/c/devices/generic:include group for generic devices
    patch:enabled remaining includes, added include guards
    
    patch:added head mounted audio to group

---
+++ modules/Core/include/CuteVR/Devices/Generic.hpp
@@ -0,0 +1,15 @@
+/// @file
+/// @author Marcus Meeßen
+/// @copyright Copyright (c) 2017-2018 Marcus Meeßen
+/// @copyright Copyright (c) 2018      MASKOR Institute FH Aachen
+
+#ifndef CUTE_VR_DEVICES_GENERIC
+#define CUTE_VR_DEVICES_GENERIC
+
+#include <CuteVR/Devices/Controller/Generic.hpp>
+#include <CuteVR/Devices/HeadMountedAudio/Generic.hpp>
+#include <CuteVR/Devices/HeadMountedDisplay/Generic.hpp>
+#include <CuteVR/Devices/Tracker/Generic.hpp>
+#include <CuteVR/Devices/TrackingReference/Generic.hpp>
+
+#endif // CUTE_VR_DEVICES_GENERIC
