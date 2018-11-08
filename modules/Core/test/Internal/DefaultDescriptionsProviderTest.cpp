141d2bbba2720eeb35ac0bba81d3b3c0c3486f56
Marcus Meeßen <marcus@meessen.online>
2018-11-08 22:56:38 +0100

    m/c/internal/defaultDescriptionProvider
    patch:implementation

---
+++ modules/Core/test/Internal/DefaultDescriptionsProviderTest.cpp
@@ -0,0 +1,19 @@
+/// @file
+/// @author Marcus Meeßen
+/// @copyright Copyright (c) 2017-2018 Marcus Meeßen
+/// @copyright Copyright (c) 2018      MASKOR Institute FH Aachen
+
+#include <QtTest/QtTest>
+
+#include <CuteVR/Internal/DefaultDescriptionsProvider.hpp>
+
+class DefaultDescriptionsProviderTest :
+        public QObject {
+Q_OBJECT
+
+private slots: // tests
+};
+
+QTEST_APPLESS_MAIN(DefaultDescriptionsProviderTest)
+
+#include "Internal/DefaultDescriptionsProviderTest.moc"
