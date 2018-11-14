50313e37d5e5c3efa3d1d96e072904a22efafecf
Marcus Meeßen <marcus@meessen.online>
2018-11-14 23:21:43 +0100

    m/c/internal/defaultHandsProvider
    patch:handling with callback, query of hand type, listen to events
    
    patch:empty test

---
+++ modules/Core/test/Internal/DefaultHandsProviderTest.cpp
@@ -0,0 +1,19 @@
+/// @file
+/// @author Marcus Meeßen
+/// @copyright Copyright (c) 2017-2018 Marcus Meeßen
+/// @copyright Copyright (c) 2018      MASKOR Institute FH Aachen
+
+#include <QtTest/QtTest>
+
+#include <CuteVR/Internal/DefaultHandsProvider.hpp>
+
+class DefaultHandsProviderTest :
+        public QObject {
+Q_OBJECT
+
+private slots: // tests
+};
+
+QTEST_APPLESS_MAIN(DefaultHandsProviderTest)
+
+#include "Internal/DefaultHandsProviderTest.moc"
