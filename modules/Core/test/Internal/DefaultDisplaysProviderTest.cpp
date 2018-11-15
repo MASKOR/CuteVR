91c842666f0961080212a83b942da5dc7c68aa01
Marcus Meeßen <marcus@meessen.online>
2018-11-15 22:05:52 +0100

    m/c/internal/defaultDisplaysProvider

---
+++ modules/Core/test/Internal/DefaultDisplaysProviderTest.cpp
@@ -0,0 +1,19 @@
+/// @file
+/// @author Marcus Meeßen
+/// @copyright Copyright (c) 2017-2018 Marcus Meeßen
+/// @copyright Copyright (c) 2018      MASKOR Institute FH Aachen
+
+#include <QtTest/QtTest>
+
+#include <CuteVR/Internal/DefaultDisplaysProvider.hpp>
+
+class DefaultDisplaysProviderTest :
+        public QObject {
+Q_OBJECT
+
+private slots: // tests
+};
+
+QTEST_APPLESS_MAIN(DefaultDisplaysProviderTest)
+
+#include "Internal/DefaultDisplaysProviderTest.moc"
