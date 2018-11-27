2f9e879bc4a43c3a6c119ae7079131490cb0182f
Marcus Meeßen <marcus@meessen.online>
2018-11-27 12:13:16 +0100

    cmake/moduleConfiguration:exported script to find module dependencies

---
+++ cmake/ModuleConfiguration.cmake
@@ -0,0 +1,10 @@
+# Copyright (c) 2017-2018 Marcus Meeßen
+# Copyright (c) 2018      MASKOR Institute FH Aachen
+#
+# CuteVR module configuration
+
+cmake_minimum_required(VERSION 3.5.1 FATAL_ERROR)
+
+@_FIND_DEPENDENCIES@
+
+include(${CMAKE_CURRENT_LIST_DIR}/@_EXPORT@.cmake)
